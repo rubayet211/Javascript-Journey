92757a3534ec7c637ef147dae6bcca722cfbccc7
rubayet211 <rubayet211@gmail.com>
2023-04-20 18:02:05 +0600

    Update head html= show  number, new code practice

---
+++ 7.js
@@ -0,0 +1,5 @@
+// A Block of Code that performs a task
+
+// Function Declaration (defining a function)
+
+//Function call (executing a function)
---
+++ Basic/6.js
@@ -0,0 +1,24 @@
+const age=18;
+//If else else if
+if(age>18){
+    console.log("You can vote");
+}
+else if(age==18){
+    console.log("You can register to vote");
+}
+else{
+    console.log("You can't vote");
+} 
+
+//While loop
+console.log("While Loop");
+let i=0;
+while(i<10){
+    console.log(i);
+    i++;
+}
+console.log("For Loop");
+//For loop
+for(let i=0;i<10;i++){
+    console.log(i);
+}
